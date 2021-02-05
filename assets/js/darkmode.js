f37d24b23738b9fd03bb77a86bacf98cf354f72c
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-02-05 11:10:50 -0600

    Add darkmode to subpages

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -4,25 +4,37 @@ let toogleStatus = document.getElementById('check-mode');
 let bodyElement = document.getElementsByTagName('body')[0];
 let navLogo = document.getElementsByClassName('nav-logo')[0];
 let cards = document.getElementsByClassName('card');
+let container = document.getElementsByClassName('page');
 
+console.log(imode);
 if(imode == "true"){
   toogleStatus.checked = true;
-  darkmode()
+  darkmode();
 }
 
 function darkmode(){
   if(toogleStatus.checked){
     bodyElement.className += " " + "darkmode";
     navLogo.className += " " + "darkmode";
-    for(let i = 0; i < cards.length; i++){
-      cards[i].className += " " + "alt-background";
+    if(cards){
+      for(let i = 0; i < cards.length; i++){
+        cards[i].className += " " + "alt-background";
+      }
+    }
+    if(container[0]){
+      container[0].className += " " + "darkmode";
     }
     localStorage.setItem("isDark", true);
   }else{
     bodyElement.classList.remove('darkmode');
     navLogo.classList.remove('darkmode');
-    for(let i = 0; i < cards.length; i++){
-      cards[i].classList.remove('alt-background');
+    if(cards){
+      for(let i = 0; i < cards.length; i++){
+        cards[i].classList.remove('alt-background');
+      }
+    }
+    if(container[0]){
+      container[0].classList.remove("darkmode");
     }
     localStorage.setItem("isDark", false);
   }
